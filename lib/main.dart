e5f982fd5a26b038ff71ecc3f0b8f47a79aafa1f
mistakemrk <arsestytania@gmail.com>
2025-06-01 13:14:11 +0900

    fix: App の title の変更。

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NMEA Viewer',
+      title: 'B-Trainer Log Viewer',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
